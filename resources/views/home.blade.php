ad83a266dbf088989d841c579f21b148c49768fe
Arief Setya <email@ariefsetya.com>
2015-12-19 16:24:33 +0700

    update dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,60 +14,60 @@
     <section class="content">
     <h1>Selamat Datang, {{Auth::user()->name}}</h1>
       <!-- Small boxes (Stat box) -->
-      <!--div class="row">
+      <div class="row">
         <div class="col-lg-3 col-xs-6">
           <div class="small-box bg-aqua">
             <div class="inner">
-              <h3>150</h3>
+              <h3>{{sizeof(\App\Anggota::where('id_koperasi',Auth::user()->assigned_koperasi)->get())}}</h3>
 
-              <p>New Members Today</p>
+              <p>Anggota</p>
             </div>
             <div class="icon">
               <i class="fa fa-users"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="{{url('anggota/baru')}}" class="small-box-footer">Tambah baru <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <div class="col-lg-3 col-xs-6">
           <div class="small-box bg-green">
             <div class="inner">
-              <h3>53<sup style="font-size:20px">%</sup></h3>
+              <h3>{{sizeof(\App\Transaksi::where('id_koperasi',Auth::user()->assigned_koperasi)->where('jenis_transaksi','Pinjaman')->get())}}</h3>
 
-              <p>Sent Mail Today</p>
+              <p>Seluruh Pinjaman</p>
             </div>
             <div class="icon">
               <i class="ion ion-stats-bars"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="{{url('transaksi/pinjaman/baru')}}" class="small-box-footer">Buat baru <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <div class="col-lg-3 col-xs-6">
           <div class="small-box bg-yellow">
             <div class="inner">
-              <h3>44</h3>
+              <h3>{{sizeof(\App\Transaksi::where('id_koperasi',Auth::user()->assigned_koperasi)->where('jenis_transaksi','Pinjaman')->where('status','Lunas')->get())}}</h3>
 
-              <p>Ticket Today</p>
+              <p>Pinjaman Lunas</p>
             </div>
             <div class="icon">
               <i class="fa fa-area-chart"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="{{url('transaksi/pinjaman')}}" class="small-box-footer">Lihat Semua <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-        <div class="col-lg-3 col-xs-6">
+        <div class="col-lg-9 col-xs-6">
           <div class="small-box bg-red">
             <div class="inner">
-              <h3>65</h3>
+              <h3>Rp. {{number_format(\App\Keuangan::selectRaw('(sum(masuk)-sum(keluar)) as saldo')->where('id_koperasi',Auth::user()->assigned_koperasi)->first()['saldo'],2,",",".")}}</h3>
 
-              <p>Open Email</p>
+              <p>Saldo Koperasi</p>
             </div>
             <div class="icon">
               <i class="ion ion-pie-graph"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="{{url('keuangan/rekap')}}" class="small-box-footer">Cek Rekap Keuangan <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-      </div-->
+      </div>
       <!-- /.row -->
       <!-- Main row -->
       <div class="row">
