ccdedadeecc855064222ce7899d98d07da96db1b
Arief Setya <ariefsetya@live.com>
2016-03-01 21:40:09 +0700

    update css

--- resources/views/transaksi/printpembayaran.blade.php
+++ resources/views/transaksi/printpembayaran.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Tanda Terima Angsuran</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="{{ url('bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ url('bootstrap/css/bootstrap.css') }}">
   <link rel="stylesheet" href="{{ url('dist/css/AdminLTE.min.css') }}">
   <link rel="stylesheet" href="{{ url('dist/css/skins/_all-skins.min.css') }}">
 </head>
